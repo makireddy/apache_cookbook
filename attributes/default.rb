afb6c98e67cb08d9642249e341b5608a7f2743ac
root <root@makireddy2.mylabserver.com>
2016-07-23 00:00:32 +0000

    apache cookbook is added

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default["apache"]["sites"]["makireddy3"] = { "site_title" => "makireddy2s website is comming sooonn", "port" => 80, "domain" => "makireddy3.mylabserver.com" }
+default["apache"]["sites"]["makireddy3b"] = { "site_title" => "makireddy2bs website is comming sooonn", "port" => 80, "domain" => "makireddy3b.mylabserver.com" }
+
